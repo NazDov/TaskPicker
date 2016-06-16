a7c8675ffe935f850f29bae43f4051f422ecf844
NazDov <nazardovhuy@gmail.com>
2016-06-17 00:14:54 +0300

    added docs

--- src/main/java/com/softserveinc/edu/ita/taskmanager/factory/TaskFactory.java
+++ src/main/java/com/softserveinc/edu/ita/taskmanager/factory/TaskFactory.java
@@ -4,11 +4,26 @@ import java.util.*;
 
 import com.softserveinc.edu.ita.taskmanager.model.AbstractTask;
 
+/**
+ * 
+ * @author Nazar Dovhyy
+ * 
+ * caching AbstarctTask implementations 
+ *
+ */
 public class TaskFactory {
 	
 	private static Map<String, Class<? extends AbstractTask>> tasks = new HashMap<>();
 
-	
+	/**
+	 * returns Task.class object by the taskId e.g. (task199)
+	 * or stores the task in the cache if returns null
+	 * 
+	 * @param taskId parameter to retrieve/store object
+	 * @param type object of AbstractTask
+	 * @see AbstractTask
+	 * @return AbstractTask object class type
+	 */
 	public static <T extends AbstractTask> Class<?> getTaskObject(String taskId, Class<T> type){
 		
 		@SuppressWarnings("unchecked")
@@ -18,9 +33,9 @@ public class TaskFactory {
 			
 			tasks.put(taskId, type);
 			
+			taskObj = tasks.get(taskId);
 		}
 		
-		taskObj = tasks.get(taskId);
 		
 		
 		return taskObj;
