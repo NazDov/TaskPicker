d0dfce0dac4f1bff1e789a387daf3de725e3076b
NazDov <nazardovhuy@gmail.com>
2016-06-17 01:43:51 +0300

    reads XML config file and saves object data into cache

---
+++ src/main/java/com/softserveinc/edu/ita/taskmanager/factory/XMLToTaskFactorySaver.java
@@ -0,0 +1,103 @@
+package com.softserveinc.edu.ita.taskmanager.factory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.logging.Log;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import ch.qos.logback.classic.Logger;
+
+public class XMLToTaskFactorySaver {
+	
+	
+	public static final String UPPER_TAG ="task";
+	public static final String ID_TAG ="id";
+	public static final String NAME_TAG ="name";
+	public static final File XML_TASKS_FILE = new File("../TaskManager/task_names.xml");
+
+	private Document document;
+	private File xmlFileName;
+	private NodeList nodeList;
+	private String idValue;
+	private String nameValue;
+
+	private XMLToTaskFactorySaver(File xmlFileName)
+			throws ParserConfigurationException, SAXException, IOException {
+
+		this.xmlFileName = xmlFileName;
+		
+		if(!xmlFileName.exists()){
+			throw new FileNotFoundException("xml file not found");
+		}
+
+		document = buildDocument();
+
+	}
+
+
+
+	private Document buildDocument() throws ParserConfigurationException,
+			SAXException, IOException {
+
+		return DocumentBuilderFactory.newInstance().newDocumentBuilder()
+				.parse(xmlFileName);
+
+	}
+
+	public static XMLToTaskFactorySaver load(File xmlFileName)
+			throws Exception {
+		return new XMLToTaskFactorySaver(xmlFileName);
+	}
+
+	public XMLToTaskFactorySaver forUpperLevelTagName(String tagName) {
+
+		nodeList = document.getElementsByTagName(tagName);
+
+		return this;
+	}
+
+	public XMLToTaskFactorySaver forTagNames(String idTag, String nameTag) {
+
+		for (int i = 0; i < nodeList.getLength(); i++) {
+
+			Node node = nodeList.item(i);
+
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				 idValue =getStringByTag(idTag, node);
+				nameValue = getStringByTag(nameTag, node);
+			}
+
+		}
+
+		return this;
+
+	}
+
+	private String getStringByTag(String idTag, Node node) {
+		return ((Element) node).getElementsByTagName(idTag).item(0).getTextContent();
+	}
+	
+	
+	public XMLToTaskFactorySaver loadToTaskFactoryCache(){
+		
+		try {
+			TaskFactory.getTaskObject(idValue, Class.forName(nameValue));
+			return this;
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("error in loadToTaskFactoryCache()");
+		}
+		
+		
+		
+	}
+
+}
