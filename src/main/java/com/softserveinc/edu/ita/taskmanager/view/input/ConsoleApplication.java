a884a32a471be0af0de704e47f275c0bd438c3cc
NazDov <nazardovhuy@gmail.com>
2016-06-17 22:42:56 +0300

    in case we need to work through console

---
+++ src/main/java/com/softserveinc/edu/ita/taskmanager/view/input/ConsoleApplication.java
@@ -0,0 +1,11 @@
+package com.softserveinc.edu.ita.taskmanager.view.input;
+
+/**
+ * Created by nazar.dovhyy on 17.06.2016.
+ *
+ *
+ * This Application implementation is designed to provide input controls via System.in console
+ *
+ */
+public class ConsoleApplication implements Application {
+}
