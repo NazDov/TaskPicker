f3c5d09e614deba686a8500b65b5cc3e0a2aa14c
NazDov <nazardovhuy@gmail.com>
2016-06-17 22:50:07 +0300

    a simple marker interface

--- src/main/java/com/softserveinc/edu/ita/taskmanager/view/input/Application.java
+++ src/main/java/com/softserveinc/edu/ita/taskmanager/view/input/Application.java
@@ -2,6 +2,8 @@ package com.softserveinc.edu.ita.taskmanager.view.input;
 
 /**
  * Created by nazar.dovhyy on 17.06.2016.
+ * a simple marker interface
  */
 public interface Application {
+    // as empty as my bank account :)
 }
