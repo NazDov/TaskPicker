a5fbf07a03375b1fc622bf39cfdcec70ecb6b2c7
NazDov <nazardovhuy@gmail.com>
2016-06-18 16:44:33 +0300

    adding gitignore

---
+++ src/main/java/com/softserveinc/edu/ita/taskmanager/view/input/validator/TestInputValidator.java
@@ -0,0 +1,13 @@
+package com.softserveinc.edu.ita.taskmanager.view.input.validator;
+
+/**
+ * Created by nazar.dovhyy on 18.06.2016.
+ */
+public class TestInputValidator {
+
+    public static void main(String [] args){
+
+        System.out.println(InputValidator.validate("199"));
+
+    }
+}
