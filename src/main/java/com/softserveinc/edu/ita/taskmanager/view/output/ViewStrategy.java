682734ef1225e2f46842c39cbc3f366ef8a2ebce
NazDov <nazardovhuy@gmail.com>
2016-06-18 16:03:16 +0300

    changes in ViewStrategy

--- src/main/java/com/softserveinc/edu/ita/taskmanager/view/output/ViewStrategy.java
+++ src/main/java/com/softserveinc/edu/ita/taskmanager/view/output/ViewStrategy.java
@@ -1,8 +1,12 @@
 package com.softserveinc.edu.ita.taskmanager.view.output;
 
+import com.softserveinc.edu.ita.taskmanager.model.AbstractTask;
+import com.softserveinc.edu.ita.taskmanager.runner.TaskRunner;
+
 public interface ViewStrategy {
 
     ViewStrategy TO_CONSOLE = new ConsoleView();
+    ViewStrategy TO_CONSOLE_TASK = new ConsoleViewTask() ;
 
     void show(Object objectToShow);
 
@@ -10,10 +14,26 @@ public interface ViewStrategy {
 
         @Override
         public void show(Object callTasks) {
+
             System.out.println(callTasks);
+
+        }
+
+
+        public void show(AbstractTask task) {
+
+            System.out.println(TaskRunner.run(task));
 
         }
 
     }
 
+     class ConsoleViewTask implements ViewStrategy{
+         @Override
+         public void show(Object objectToShow) {
+             if(objectToShow instanceof AbstractTask) {
+                 System.out.println("result: "+TaskRunner.run((AbstractTask) objectToShow));
+             }
+         }
+     }
 }
