102e921bae8e31c8be6f02c119fa96c955a8ddd3
NazDov <nazardovhuy@gmail.com>
2016-06-18 16:04:47 +0300

    added exception handling in case Task is not an instance of AbstractTask

--- src/main/java/com/softserveinc/edu/ita/taskmanager/controller/XMLInitializeContext.java
+++ src/main/java/com/softserveinc/edu/ita/taskmanager/controller/XMLInitializeContext.java
@@ -1,111 +1,118 @@
 package com.softserveinc.edu.ita.taskmanager.controller;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import com.softserveinc.edu.ita.taskmanager.factory.TaskFactory;
 import com.softserveinc.edu.ita.taskmanager.model.AbstractTask;
+import com.softserveinc.edu.ita.taskmanager.model.exceptions.TaskObjectNotAbstractTaskException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
- * 
  * class designed to read XML file task_config.xml and load values (id and name)
  * into the TaskFactory cache
- * 
- * @author Nazar Dovhyy
  *
+ * @author Nazar Dovhyy
  */
 public class XMLInitializeContext {
 
-	public static final String TAG_ATTR = "task";
-	public static final String ID_TAG = "id";
-	public static final String NAME_TAG = "name";
-	public static final String DESCR_TAG = "description";
-	public static final File XML_TASKS_FILE = new File(
-			"task_config.xml");
+    public static final String TAG_ATTR = "task";
+    public static final String ID_TAG = "id";
+    public static final String NAME_TAG = "name";
+    public static final String DESCR_TAG = "description";
+    public static final File XML_TASKS_FILE = new File(
+            "task_config.xml");
 
-	private Document document;
-	private File xmlFileName;
-	private NodeList nodeList;
-	private String idValue;
-	private String nameValue;
-	private String descrValue;
+    private Document document;
+    private File xmlFileName;
+    private NodeList nodeList;
+    private String idValue;
+    private String nameValue;
+    private String descrValue;
 
-	private XMLInitializeContext(File xmlFileName)
-			throws ParserConfigurationException, SAXException, IOException {
+    private XMLInitializeContext(File xmlFileName)
+            throws ParserConfigurationException, SAXException, IOException {
 
-		this.xmlFileName = xmlFileName;
+        this.xmlFileName = xmlFileName;
 
-		if (!xmlFileName.exists()) {
-			throw new FileNotFoundException("xml file not found");
-		}
+        if (!xmlFileName.exists()) {
+            throw new FileNotFoundException("xml file not found");
+        }
 
-		document = buildDocument();
+        document = buildDocument();
 
-	}
+    }
 
-	private Document buildDocument() throws ParserConfigurationException,
-			SAXException, IOException {
+    private Document buildDocument() throws ParserConfigurationException,
+            SAXException, IOException {
 
-		return DocumentBuilderFactory.newInstance().newDocumentBuilder()
-				.parse(xmlFileName);
+        return DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                .parse(xmlFileName);
 
-	}
+    }
 
-	public static XMLInitializeContext init(File xmlFileName) throws Exception {
-		return new XMLInitializeContext(xmlFileName);
-	}
+    public static XMLInitializeContext init(File xmlFileName) throws Exception {
+        return new XMLInitializeContext(xmlFileName);
+    }
 
-	public XMLInitializeContext parseTag(String tagName) {
+    public XMLInitializeContext parseTag(String tagName) {
 
-		nodeList = document.getElementsByTagName(tagName);
+        nodeList = document.getElementsByTagName(tagName);
 
-		return this;
-	}
+        return this;
+    }
 
-	public XMLInitializeContext loadIntoCache() {
+    public XMLInitializeContext loadIntoCache() throws TaskObjectNotAbstractTaskException {
 
-		for (int i = 0; i < nodeList.getLength(); i++) {
+        for (int i = 0; i < nodeList.getLength(); i++) {
 
-			Node node = nodeList.item(i);
+            Node node = nodeList.item(i);
 
-			if (node.getNodeType() == Node.ELEMENT_NODE) {
-				idValue = getStringByTag(ID_TAG, node);
-				nameValue = getStringByTag(NAME_TAG, node);
-				descrValue = getStringByTag(DESCR_TAG, node);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                idValue = getStringByTag(ID_TAG, node);
+                nameValue = getStringByTag(NAME_TAG, node);
+                descrValue = getStringByTag(DESCR_TAG, node);
 
-			}
-			
-			loadToTaskFactoryCache(idValue, nameValue, descrValue);
-		}
+            }
 
-		return this;
+            loadToTaskFactoryCache(idValue, nameValue, descrValue);
+        }
 
-	}
+        return this;
 
-	private String getStringByTag(String idTag, Node node) {
-		return ((Element) node).getElementsByTagName(idTag).item(0)
-				.getTextContent();
-	}
+    }
 
-	private XMLInitializeContext loadToTaskFactoryCache(String idVal, String nameVal, String descrVal) {
+    private String getStringByTag(String idTag, Node node) {
+        return ((Element) node).getElementsByTagName(idTag).item(0)
+                .getTextContent();
+    }
 
-		try {
-			TaskFactory.getTaskById(idVal, (Class<? extends AbstractTask>) Class.forName(nameVal),
-					descrVal);
-			return this;
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("error in loadToTaskFactoryCache()");
-		}
+    private XMLInitializeContext loadToTaskFactoryCache(String idVal, String nameVal, String descrVal) throws TaskObjectNotAbstractTaskException {
 
-	}
+        try {
+            final Class<?> className = Class.forName(nameVal);
+
+
+            if (className.getSuperclass() == AbstractTask.class) {
+
+                TaskFactory.getTaskById(idVal, (Class<? extends AbstractTask>) className,
+                        descrVal);
+
+            } else {
+                throw new TaskObjectNotAbstractTaskException(""+className);
+            }
+            return this;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("error in loadToTaskFactoryCache()");
+        }
+
+    }
 
 }
