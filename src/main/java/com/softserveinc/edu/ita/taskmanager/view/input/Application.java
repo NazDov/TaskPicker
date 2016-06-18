4e7797817428fa2cf63b761e8ad30860733c3d62
NazDov <nazardovhuy@gmail.com>
2016-06-18 15:59:58 +0300

    changes in Application

--- src/main/java/com/softserveinc/edu/ita/taskmanager/view/input/Application.java
+++ src/main/java/com/softserveinc/edu/ita/taskmanager/view/input/Application.java
@@ -4,7 +4,16 @@ package com.softserveinc.edu.ita.taskmanager.view.input;
  * Created by nazar.dovhyy on 17.06.2016.
  * a simple marker interface
  */
-public interface Application {
-    void start();
+public interface  Application {
+
+
+     Application NULL_APPLICATION = new Application() {
+        @Override
+        public void start() {
+            // return nothing
+        }
+    };
+
+     void start();
 
 }
